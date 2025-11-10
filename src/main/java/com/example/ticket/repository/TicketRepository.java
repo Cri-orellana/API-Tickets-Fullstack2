119a54282f09db427567ea5c60ce96cd6b6457f8
Alumno Vina del Mar <vina@duoc.acad>
2025-11-10 16:34:19 -0300

    first commit

---
+++ src/main/java/com/example/ticket/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.example.ticket.repository;
+
+import com.example.ticket.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+}
---
+++ src/main/java/com/example/ticket/service/TicketService.java
@@ -0,0 +1,12 @@
+package com.example.ticket.service;
+
+import com.example.ticket.model.Ticket;
+import com.example.ticket.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TicketService {
+    
+}
