99ebf20c4731105ad6f24293c2111c6e00cf58fc
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-11-14 13:09:17 -0300

    Terminada base de la API tickets, CRUD hecho

--- src/main/java/com/example/ticket/controller/TicketController.java
+++ src/main/java/com/example/ticket/controller/TicketController.java
@@ -15,5 +15,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/tickets")
 @Tag(name = "Administracion Ticket", description = "CRUD Tickets")
 public class TicketController {
-    
+    @Autowired
+    private TicketService ticketService;
+
+    @GetMapping
+    @Operation(summary = "Obtener todos los tickets", description = "Devuelve una lista de todos los tickets disponibles")
+    public List<Ticket> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/{ticket}")
+    @Operation(summary = "Obtener un ticket por ID", description = "Devuelve un ticket específico basado en su ID")
+    public Ticket getTicketById(@PathVariable("ticket") Integer id) {
+        return ticketService.getTicketById(id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Crear un nuevo ticket", description = "Crea un nuevo ticket con la información proporcionada")
+    public Ticket createTicket(@RequestBody Ticket ticket) {
+        return ticketService.createTicket(ticket);
+    }
+
+    @PutMapping("/{ticket}")
+    @Operation(summary = "Actualizar un ticket existente", description = "Actualiza la información del ticket")
+    public Ticket updateTicket(@PathVariable("ticket") Integer id, @RequestBody Ticket ticket) {
+        Ticket ticketRegistrado = ticketService.getTicketById(id);
+        if (ticketRegistrado != null) {
+            ticketRegistrado.setDescripcion(ticket.getDescripcion());
+            ticketRegistrado.setEstado(ticket.getEstado());
+            return ticketService.updateTicket(ticketRegistrado);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{ticket}")
+    @Operation(summary = "Eliminar un ticket", description = "Elimina un ticket específico basado en su ID")
+    public void deleteTicket(@PathVariable("ticket") Integer id) {
+        ticketService.deleteTicket(id);
+    }
 }
--- src/main/java/com/example/ticket/service/TicketService.java
+++ src/main/java/com/example/ticket/service/TicketService.java
@@ -2,11 +2,35 @@ package com.example.ticket.service;
 
 import com.example.ticket.model.Ticket;
 import com.example.ticket.repository.TicketRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class TicketService {
-    
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getTicketById(Integer id) {
+        return ticketRepository.findById(id).orElse(null);
+    }
+
+    public Ticket createTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    public Ticket updateTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    public void deleteTicket(Integer id) {
+        ticketRepository.deleteById(id);
+    }
 }
